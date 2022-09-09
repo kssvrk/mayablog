c0e1abcac35f80a7769f1bcc46d3867c6f4fda9e
radha krishna <kssvrk@gmail.com>
2022-09-09 08:49:44 +0530

    basic version

---
+++ blog/blog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-dw-d8!-6u(yourywung#qhv4v%e@cvl8=s99rs4je=6g(5i5h("
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ blog/movies/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+# Create your models here.
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+from wagtail.search import index
+
+
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
+class MoviePageTag(TaggedItemBase):
+    content_object = ParentalKey('movies.MoviePage', on_delete=models.CASCADE, related_name='tagged_items')
+
+class MovieIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro')
+    ]
+
+class MoviePage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    rating = models.IntegerField(null=True,blank=True,choices=[(i, i) for i in range(1, 11)])
+    body = RichTextField(blank=True)
+    tags = ClusterTaggableManager(through=MoviePageTag, blank=True)
+    feed_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('rating'),
+        FieldPanel('body'),
+        FieldPanel('feed_image'),
+        
+        FieldPanel('tags'),
+    ]
+
+    @property
+    def get_nonrating(self):
+        
+        return 10-self.rating
